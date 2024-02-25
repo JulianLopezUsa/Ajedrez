4684e2449b208e97741403a7983ab3728314bd28
JulianLopezUsa <julian.lopez@usa.edu.co>
2024-02-25 17:41:56 -0500

    Cambios

--- src/ajedrez/Ajedrez.java
+++ src/ajedrez/Ajedrez.java
@@ -4,7 +4,6 @@ import javax.swing.SwingUtilities;
 import controlador.EventosVentanaInicio;
 import vista.MenuA;
 
-
 public class Ajedrez {
 
     public static void main(String[] args) {
@@ -12,13 +11,8 @@ public class Ajedrez {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 MenuA menu = new MenuA();
-                new EventosVentanaInicio(menu); 
+                new EventosVentanaInicio(menu);
             }
         });
     }
 }
-
-
-
-                
-                
